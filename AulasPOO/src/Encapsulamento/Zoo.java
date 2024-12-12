9c1b3c1682c8fe7002c32a1e46efd95010e4e5de
alinelima30 <alimary1994@gmail.com>
2024-12-12 11:34:21 -0300

    get e set/sobrecarga

--- AulasPOO/src/Encapsulamento/Zoo.java
+++ AulasPOO/src/Encapsulamento/Zoo.java
@@ -2,7 +2,8 @@ package Encapsulamento;
 public class Zoo {
 	public static void main(String[] args) {
 		Animal bicho = new Animal();
-		
+		bicho.setNome("toto");
+		System.out.println(bicho.getNome());
 
 	}
 
